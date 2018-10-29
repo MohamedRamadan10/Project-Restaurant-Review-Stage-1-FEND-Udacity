91ad286938ab45df9643481a8d638cea003bbf6c
MohamedRamadan10 <32428980+MohamedRamadan10@users.noreply.github.com>
2018-10-29 13:07:18 -1000

    Add files via upload

---
+++ js/register.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6 */
+/* Set up service worker */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js', {
+      scope: "/"
+    })
+    .then(reg => {
+      console.log('Service Worker Registration Successful: ' + reg.scope);
+    })
+    .catch(error => {
+      console.log('Service Worker Registration Failed: ' + error);
+    });
+}
+
